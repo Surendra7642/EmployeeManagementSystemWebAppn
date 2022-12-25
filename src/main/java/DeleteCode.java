3432ec4141bfab8433424af80c18cbc48461714a
Surendra7642 <119400689+Surendra7642@users.noreply.github.com>
2022-12-25 15:31:44 +0530

    Update DeleteCode.java

--- src/main/java/DeleteCode.java
+++ src/main/java/DeleteCode.java
@@ -11,7 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//This is pulling
+//This is pulling Surye
 /**
  * Servlet implementation class DeleteCode
  */
